8b52c310cf482bf33b5a0700cc0887ec4529530e
DngYnQ <dngynq@gmail.com>
2025-05-27 00:12:21 +0300

    client udp check fix

--- client.py
+++ client.py
@@ -29,16 +29,32 @@ def udp():
         return (data * 1.0).astype(np.float32)
     def recv():
         while True:
-            data = udp_sock.recv(CHUNK * 10)
-            stream.write(OGG_2_RAW(data), CHUNK)
+            try:
+                data = udp_sock.recv(CHUNK * 10)
+                stream.write(OGG_2_RAW(data), CHUNK)
+            except:return 0
     def send():
         while True:
-            data = stream.read(CHUNK, exception_on_overflow=False)
-            data = RAW_2_OGG(data)
-            udp_sock.sendall(data)
+            try:
+                data = stream.read(CHUNK, exception_on_overflow=False)
+                data = RAW_2_OGG(data)
+                udp_sock.sendall(data)
+            except:return 0
 
-    threading.Thread(target=recv).start()
-    threading.Thread(target=send).start()
+    def check():
+        while True:
+            if not recv_th.is_alive() or not send_th.is_alive():
+                print("udp  : voice   : not work  - died")
+                return 0
+
+    recv_th = threading.Thread(target=recv)
+    recv_th.start()
+    send_th=threading.Thread(target=send)
+    send_th.start()
+    check_th = threading.Thread(target=check)
+    check_th.start()
+
+
 
 def tcp():
     ...
